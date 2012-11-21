5956153697a06dee8b7c8442bb5f5def8fd1ade6
rvanderv <ryan.vanderv@gmail.com>
2012-11-21 11:55:24 -0700

    PlaceDetail added codez

--- src/com/iwantfood/ryanvanderveen/Places/PlaceDetail.java
+++ src/com/iwantfood/ryanvanderveen/Places/PlaceDetail.java
@@ -1,5 +1,26 @@
 package com.iwantfood.ryanvanderveen.Places;
 
-public class PlaceDetail {
+import java.io.Serializable;
 
+import com.google.api.client.util.Key;
+ 
+/** Implement this class from "Serializable"
+* So that you can pass this class Object to another using Intents
+* Otherwise you can't pass to another actitivy
+* */
+public class PlaceDetail implements Serializable {
+ 
+    @Key
+    public String status;
+ 
+    @Key
+    public Place result;
+ 
+    @Override
+    public String toString() {
+        if (result!=null) {
+            return result.toString();
+        }
+        return super.toString();
+    }
 }
